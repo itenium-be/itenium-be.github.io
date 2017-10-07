c863bd73bbcae3c60d90d8ee204fc0d128618307
Laoujin <woutervs@hotmail.com>
2017-10-07 13:31:33 +0200

    tech stack

--- js/script.js
+++ js/script.js
@@ -25,17 +25,17 @@ jQuery(function ($) {
       jQuery('.search-open').fadeOut(500);
       jQuery('.search-btn').addClass('fa-search');
       jQuery('.search-btn').removeClass('fa-times');
-    }   
-  }); 
+    }
+  });
 
 
- 
+
     // -------------------------------------------------------------
 
     //---------------------------------------------------------------
-    
+
     (function () {
-    
+
         /* initialize shuffle plugin */
         var $grid = $('#grid');
 
@@ -57,16 +57,16 @@ jQuery(function ($) {
             // reshuffle grid
             $grid.shuffle('shuffle', groupName );
         });
-    
-    }());   
-    
+
+    }());
+
 // -------------------------------------------------------------
 //                ADD CLASS
 // -------------------------------------------------------------
 
 
-$(window).scroll(function(){ 
-  if ($(this).scrollTop() >670){      
+$(window).scroll(function(){
+  if ($(this).scrollTop() >670){
     $('.who-we-are').addClass("menu-fix");
   } else{
     $('.who-we-are').removeClass("menu-fix");
@@ -136,7 +136,7 @@ $(window).scroll(function(){
 
 
    jQuery(document).ready(function ($) {
-    
+
     var offset = 220;
     var duration = 500;
     $(window).scroll(function () {
@@ -164,9 +164,9 @@ $(window).scroll(function(){
 
 //Rs-PLUGIN
 jQuery(document).ready(function() {
-                
-                    
-                                
+
+
+
                     jQuery('.tp-banner').show().revolution(
                     {
                         dottedOverlay:"none",
@@ -174,29 +174,29 @@ jQuery(document).ready(function() {
                         startwidth:1170,
                         startheight:670,
                         hideThumbs:200,
-                        
+
                         thumbWidth:100,
                         thumbHeight:50,
                         thumbAmount:5,
-                        
+
                         navigationType:"none",
                         navigationArrows:"solo",
                         navigationStyle:"preview4",
-                        
+
                         touchenabled:"on",
                         onHoverStop:"off",
-                        
+
                         swipe_velocity: 0.7,
                         swipe_min_touches: 1,
                         swipe_max_touches: 1,
                         drag_block_vertical: false,
-                                                
+
                                                 parallax:"mouse",
                         parallaxBgFreeze:"on",
                         parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
-                                                
+
                         keyboardNavigation:"off",
-                        
+
                         navigationHAlign:"center",
                         navigationVAlign:"bottom",
                         navigationHOffset:0,
@@ -211,41 +211,41 @@ jQuery(document).ready(function() {
                         soloArrowRightValign:"center",
                         soloArrowRightHOffset:20,
                         soloArrowRightVOffset:0,
-                                
+
                         shadow:0,
                         fullWidth:"on",
                         fullScreen:"off",
 
                         spinner:"spinner4",
-                        
+
                         stopLoop:"off",
                         stopAfterLoops:-1,
                         stopAtSlide:-1,
 
                         shuffle:"off",
-                        
-                        autoHeight:"off",                       
-                        forceFullWidth:"off",                       
-                                                
-                                                
-                                                
+
+                        autoHeight:"off",
+                        forceFullWidth:"off",
+
+
+
                         hideThumbsOnMobile:"off",
-                        hideNavDelayOnMobile:1500,                      
+                        hideNavDelayOnMobile:1500,
                         hideBulletsOnMobile:"off",
                         hideArrowsOnMobile:"off",
                         hideThumbsUnderResolution:0,
-                        
+
                         hideSliderAtLimit:0,
                         hideCaptionAtLimit:0,
                         hideAllCaptionAtLilmit:0,
                         startWithSlide:0,
                         videoJsPath:"rs-plugin/videojs/",
-                        fullScreenOffsetContainer: ""   
+                        fullScreenOffsetContainer: ""
                     });
-                    
-                    
-                    
-                                    
+
+
+
+
                 });
 
 
@@ -266,7 +266,7 @@ $(function() {
                     {
                         var href = $(this).attr( 'href' );
 
-                        //  if the clicked link is linked to an anchor, scroll the page to that anchor 
+                        //  if the clicked link is linked to an anchor, scroll the page to that anchor
                         if ( href.slice( 0, 1 ) == '#' )
                         {
                             $menu.one(
@@ -278,9 +278,9 @@ $(function() {
                                         {
                                             $html.animate({
                                                 scrollTop: $( href ).offset().top
-                                            }); 
+                                            });
                                         }, 10
-                                    );  
+                                    );
                                 }
                             );
                         }
